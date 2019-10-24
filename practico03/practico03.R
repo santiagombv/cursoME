bcae53fd4f02b7664f6b1293254a0e4f51b4f267
santiagombv <santiagombv@gmail.com>
2019-10-23 22:50:13 -0300

    general update for TP3

--- practico03/practico03.R
+++ practico03/practico03.R
@@ -5,6 +5,8 @@
 ###############
 
 library(mgcv) 
+library(tidymv)
+
 RIKZ <- read.table("RIKZ.txt", header = TRUE)
 
 #para calcular la riqueza sumamos las filas 2 a la 76
@@ -59,7 +61,7 @@ vis.gam(fit1, plot.type = "persp", theta = 55, type = "response")
 # Utilizando predict
 layout(matrix(1:3,1,3)) # se esperan 3 splines
 
-#1 para exposure = 8
+# 1 para exposure = 8
 A<-seq(min(RIKZ$NAP), max(RIKZ$NAP), by =0.01)  
 E<-rep("8", length(A))				
 nr8<-data.frame(exposure = E, NAP = A)
@@ -69,7 +71,7 @@ points(nr8$NAP, Y8$fit + Y8$se.fit, type = "l", lty = 3)
 points(nr8$NAP, Y8$fit - Y8$se.fit, type = "l", lty = 3)
 title("exposure 8")
 
-#para exposure=10
+# para exposure=10
 A <- seq(min(RIKZ$NAP), max(RIKZ$NAP), by = 0.01)  
 E <- rep("10", length(A))				
 nr10 <- data.frame(exposure=E, NAP=A)
@@ -79,7 +81,7 @@ points(nr10$NAP, Y10$fit + Y10$se.fit, type = "l", lty = 3)
 points(nr10$NAP, Y10$fit - Y10$se.fit, type = "l", lty = 3)
 title("exposure 10")
 
-#para exposure= 11
+# para exposure= 11
 A <- seq(min(RIKZ$NAP), max(RIKZ$NAP), by = 0.01)  
 E <- rep("11", length(A))				
 nr11 <- data.frame(exposure=E, NAP=A)
@@ -88,6 +90,22 @@ plot(nr11$NAP, Y11$fit, type = "l", xlab = "NAP", ylab = "riqueza", ylim = c(0,2
 points(nr10$NAP, Y11$fit + Y10$se.fit, type = "l", lty = 3)
 points(nr10$NAP, Y11$fit - Y10$se.fit, type = "l", lty = 3)
 title("exposure 11")
+
+# gráficos "tidy style" (tidymv package)
+# modelo 1
+plot_smooths(fit1, series = NAP, comparison = exposure)
+
+g1 <- plot_smooths(fit1, series = NAP, comparison = exposure, transform = exp) 
+g1 <- g1 + scale_fill_manual(values = c("red", "orange", "yellow"))
+g1 <- g1 + scale_color_manual(values = c("red", "orange", "yellow"))
+g1 + theme_bw() + theme(legend.position = "top")
+
+# modelo 2
+g1 <- plot_smooths(fit2, series = NAP, comparison = exposure, transform = exp) 
+g1 <- g1 + scale_fill_manual(values = c("red", "orange", "yellow"))
+g1 <- g1 + scale_color_manual(values = c("red", "orange", "yellow"))
+g1 + theme_bw() + theme(legend.position = "top")
+
 
 ##############
 ### Caso 2 ###
@@ -110,14 +128,26 @@ gam.check(m1)
 layout(1)
 
 # gráfico
-layout(matrix(1:2,1,2))
+layout(matrix(1:2,1,2)) # se esparan 2 splines
 plot(m1)
 layout(1)
 
+# gráfico para superficies via mgcv
 vis.gam(m1, view = c("nec", "flo"), theta = -45, ticktype = "detailed",
         type = "response")
 
-vis.gam(m1, view = c("nec", "flo"), type = "response", plot.type = "contour")
+vis.gam(m1, view = c("nec", "flo"), type = "response",
+        plot.type = "contour")
+
+# gráfico para superficies via tidymv
+m1_p <- predict_gam(m1)
+m1_p %>%
+  ggplot(aes(nec, flo, z = exp(fit))) + # invertir enlace
+  geom_raster(aes(fill = exp(fit))) + 
+  geom_contour(colour = "white") +
+  scale_fill_viridis(name = "pol", option = "viridis") + # nuevo!
+  theme_minimal()
+
 
 # modelos isotrópico con interacción
 m2 <- gam(pol ~ s(nec, flo, k = 100, bs = "tp"), data=cyc, 
@@ -126,6 +156,15 @@ summary(m2)
 
 vis.gam(m2, view = c("nec", "flo"), type = "response", 
         plot.type = "contour", color = "cm")
+
+# gráfico para superficies via tidymv
+m2_p <- predict_gam(m2)
+m2_p %>%
+        ggplot(aes(nec, flo, z = exp(fit))) + # invertir enlace
+        geom_raster(aes(fill = exp(fit))) + 
+        geom_contour(colour = "white") +
+        scale_fill_viridis(name = "pol", option = "viridis") + # nuevo!
+        theme_minimal()
 
 # comparación
 BIC(m1, m2)
@@ -168,5 +207,30 @@ vis.gam(m5, plot.type = "contour", color = "cm", type = "response",
 points(cyc$nec, cyc$flo, pch = 20)
 
 layout(1)
+
+# gráficos "tidy sytile" via tidymv
+m3_p <- predict_gam(m3)
+m3_p %>%
+        ggplot(aes(nec, flo, z = exp(fit))) + # invertir enlace
+        geom_raster(aes(fill = exp(fit))) + 
+        geom_contour(colour = "white") +
+        scale_fill_viridis(name = "pol", option = "viridis") + # nuevo!
+        theme_minimal()
+
+m4_p <- predict_gam(m4)
+m4_p %>%
+        ggplot(aes(nec, flo, z = exp(fit))) + # invertir enlace
+        geom_raster(aes(fill = exp(fit))) + 
+        geom_contour(colour = "white") +
+        scale_fill_viridis(name = "pol", option = "viridis") + # nuevo!
+        theme_minimal()
+
+m5_p <- predict_gam(m5)
+m5_p %>%
+        ggplot(aes(nec, flo, z = exp(fit))) + # invertir enlace
+        geom_raster(aes(fill = exp(fit))) + 
+        geom_contour(colour = "white") +
+        scale_fill_viridis(name = "pol", option = "viridis") + # nuevo!
+        theme_minimal()
 
 ### END ###
